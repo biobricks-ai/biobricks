f1004972bdcac5aad4a50ef648cca801ad797c57
Thomas Luechtefeld <tom@insilica.co>
2023-01-11 12:01:01 -0500

    fixed issue with loading function

--- biobricks/load.py
+++ biobricks/load.py
@@ -3,36 +3,33 @@ import types, pyarrow.parquet as pq, re
 from pathlib import Path
 from logger import logger
 
-def _buildns(paths):
-    namespace = types.SimpleNamespace()
-    pkey = lambda p: re.sub(r'[.-]','_',p)
-    for p in paths:
-        path = p.relative_to(dir)
-        logger.info(f"loading {path}...")
-        current = namespace
-        for part in path.parts:
-            key = pkey(part)
-            if not hasattr(current, key) and part.endswith('.parquet'):
-                setattr(current, key[:-8], pq.read_table(p))
-            elif not hasattr(current, key):
-                setattr(current, key, types.SimpleNamespace())
-                current = getattr(current, key)
-            elif hasattr(current, key):
-                current = getattr(current, key)
-    logger.info(f"loaded {len(paths)} tables")
-    return namespace
-    
 def load(brick,org="biobricks-ai"):
     bdir = bblib() / org / brick
     if not bdir.exists(): 
         raise Exception(f"no path '{bdir}' try `biobricks.pull({brick})`")
        
-    def list_parquet_subdirs(dir: Path):
+    def dirns(dir: Path):
         filter = lambda d: d.is_dir() and d.name.endswith('.parquet')
-        return [d for d in dir.rglob('*') if filter(d)]
+        paths = [d for d in dir.rglob('*') if filter(d)]
+        namespace = types.SimpleNamespace()
+        pkey = lambda p: re.sub(r'[.-]','_',p)
+        for p in paths:
+            path = p.relative_to(dir)
+            logger.info(f"loading {path}...")
+            current = namespace
+            for part in path.parts:
+                key = pkey(part)
+                if not hasattr(current, key) and part.endswith('.parquet'):
+                    setattr(current, key[:-8], pq.read_table(str(p)))
+                elif not hasattr(current, key):
+                    setattr(current, key, types.SimpleNamespace())
+                    current = getattr(current, key)
+                elif hasattr(current, key):
+                    current = getattr(current, key)
+        logger.info(f"loaded {len(paths)} tables from {dir}")
+        return namespace
 
-    pathD = list_parquet_subdirs(bdir / 'data')
-    pathB = list_parquet_subdirs(bdir / 'brick')
-    return _buildns(pathD + pathB)
-
-
+    ns1 = dirns(bdir / 'data')
+    ns2 = dirns(bdir / 'brick')
+    result = {**ns2.__dict__, **ns1.__dict__}
+    return types.SimpleNamespace(**result)
