60f6153dc75b4c428566dc3375b9ae932e0a0ec7
Thomas Luechtefeld <tom@insilica.co>
2023-02-23 15:03:46 -0500

    fixed symlink check

--- biobricks/checks.py
+++ biobricks/checks.py
@@ -18,15 +18,13 @@ def check_token(token, silent=False):
         raise ValueError(f"Invalid token. Run `biobricks configure`")
     return is_valid
 
-import os
-
 def check_symlink_permission():
     try:
         src = tempfile.NamedTemporaryFile()
         dst = os.path.join(tempfile.gettempdir(), str(uuid.uuid4()))
         os.symlink(src.name, dst)
-        os.remove(src.name)
         os.remove(dst)
+        src.close()
         return True
     except Exception as e:
         raise PermissionError("Need Symlink Permission. Contact administrator and see https://dvc.org/doc/user-guide/how-to/run-dvc-on-windows#enable-symbolic-links.")
