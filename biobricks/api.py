8f4de3334c5e4555f82074fc0c018d5470dd0578
Thomas Luechtefeld <tom@insilica.co>
2023-02-23 22:01:31 -0500

    added check for configuration to api

--- biobricks/api.py
+++ biobricks/api.py
@@ -1,13 +1,15 @@
 from .brick import Brick
 from .cli import cli
-from .checks import check_configured()
+from .checks import check_configured
 
 def load(brick):
     """Load a brick from the local filesystem"""
+    check_configured()
     return Brick.Resolve(brick).load()
 
 def install(brick):
     """Install a brick from a remote repository"""
+    check_configured()
     return Brick.Resolve(brick).install()
 
 def configure():
