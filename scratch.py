9178591250154c622f69f1b3d67b87328a9031ed
Thomas Luechtefeld <tom@insilica.co>
2023-10-07 21:18:24 -0400

    removed accidentally commited scratch.py and ignored it

--- scratch.py
+++
@@ -1,6 +0,0 @@
-import biobricks as bb
-from pyspark.sql import SparkSession
-
-spark = SparkSession.builder.appName("pubmed").getOrCreate()
-df = spark.read.parquet(bb.assets("pubmed")[0])
-df.limit(10).show()
